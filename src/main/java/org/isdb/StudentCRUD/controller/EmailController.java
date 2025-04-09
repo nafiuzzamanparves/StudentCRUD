ed6d9932681889c060796530c420f29fb590ef49
Nafiuzzaman Parves <nafiuzzaman.parves@gmail.com>
2025-04-09 12:27:11 +0600

    Send mail

---
+++ src/main/java/org/isdb/StudentCRUD/controller/EmailController.java
@@ -0,0 +1,31 @@
+package org.isdb.StudentCRUD.controller;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+import org.isdb.StudentCRUD.model.EmailRequest;
+import org.isdb.StudentCRUD.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.mail.MessagingException;
+
+@RestController
+public class EmailController {
+
+	@Autowired
+	private EmailService emailService;
+
+	@PostMapping("/send-email")
+	public ResponseEntity<String> sendEmail(@RequestBody EmailRequest request) {
+		try {
+			emailService.sendEmail(request.getTo(), request.getSubject(), request.getBody());
+			return ResponseEntity.ok("Email sent successfully");
+		} catch (MessagingException | IOException | GeneralSecurityException e) {
+			return ResponseEntity.internalServerError().body("Failed to send email: " + e.getMessage());
+		}
+	}
+}
